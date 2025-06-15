11dec39e49c153dbf3f82dc85db882b814c6d338
Kamil Kolanowski <tanssive@yahoo.com>
2025-06-15 13:31:57 +0200

    Testing react

--- React.ExternalAPI.KamilKolanowski/cats-web/src/App.js
+++ React.ExternalAPI.KamilKolanowski/cats-web/src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
+
+const products = [
+  { title: 'Cabbage', isFruit: false, id: 1 },
+  { title: 'Garlic', isFruit: false, id: 2 },
+  { title: 'Apple', isFruit: true, id: 3 },
+];
+
+const listItems = products.map(product =>
+  <li key={product.id} style={{
+    color: product.isFruit ? 'magenta' : 'darkgreen'
+  }}>
+    {product.title}
+  </li>
+)
+
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <MyButton count={count} onClick={handleClick} />
+        <MyButton count={count} onClick={handleClick} />
+        <ul>
+          {listItems}
+        </ul>
+      </div>
   );
 }
 
-export default App;
+function MyButton({ count, onClick }) {
+  return (
+      <>
+        <button onClick={onClick}>Clicked {count} times.</button>
+      </>
+  )
+}
